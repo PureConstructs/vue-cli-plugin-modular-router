a23d0bfedb8986f066a86b5a3221544827a38291
Eric Cormier <Eric@Pureconstructs.com>
2018-10-11 12:18:05 -0600

    feat(generator): inject import declarations
    - add code to inject import declarations
    - install recast dependency
    
    refs(#3)

--- generator/index.js
+++ generator/index.js
@@ -3,11 +3,11 @@ const path = require('path')
 const recast = require('recast')
 
 module.exports = (api, options, rootOptions, invoking) => {
-  const routeRootDir = './src/router'
+  const routerRootDir = './src/router'
   const viewsRootDir = './src/views'
-  const routeRootFile = './src/router/index.js'
+  const routerRootFile = './src/router/index.js'
   const templatesRoot = './templates'
-  const moduleDir = path.join(routeRootDir, `${options.moduleName}`)
+  const moduleDir = path.join(routerRootDir, `${options.moduleName}`)
   const viewsDir = path.join(viewsRootDir, `${options.moduleName}`)
 
   // Verify router plugin installed
@@ -30,15 +30,50 @@ module.exports = (api, options, rootOptions, invoking) => {
 
   api.exitLog('Rendered template files', 'info')
 
-  // Verify routeRootFile exists, if not Render
-  if (!fs.existsSync(`${routeRootFile}`)) {
+  // Verify routerRootFile exists, if not Render
+  if (!fs.existsSync(`${routerRootFile}`)) {
     api.render({
-      [`${routeRootFile}`]: `${templatesRoot}/index.js`
+      [`${routerRootFile}`]: `${templatesRoot}/index.js`
     })
     api.exitLog('Router root file rendered', 'info')
   }
 
   api.onCreateComplete((files) => {
+    // inject new module name into the root router file
+    const code = fs.readFileSync(`${routerRootFile}`, 'utf8')
+    const ast = recast.parse(code)
+
+    // New option expression to inject
+
+    // New Import Declaration to inject
+    const imports = [`import ${options.moduleName}Routes from '@/router/${options.moduleName}'`]
+
+    // Inject new Import Declaration
+    const toImport = i => recast.parse(`${i}\n`).program.body[0]
+    const importDeclarations = []
+
+    recast.types.visit(ast, {
+      visitImportDeclaration({
+        node
+      }) {
+        lastImportIndex = ast.program.body.findIndex(n => n === node)
+        importDeclarations.push(node)
+        return false
+      }
+    })
+    delete ast.program.body[lastImportIndex].loc
+
+    const newImport = imports.map(toImport)
+    ast.program.body.splice(lastImportIndex + 1, 0, ...newImport)
+
+    // write code injections to file
+    fs.writeFile(`${routerRootFile}`, recast.print(ast).code, (err) => {
+      if (err) {
+        api.exitLog('Error writing to router root index.js: ' + err, 'error')
+        return
+      }
+    })
+
     // Remove router.js generated from initial router plugin installation
     if (fs.existsSync('./src/router.js')) {
       fs.unlink('./src/router.js', (err) => {
