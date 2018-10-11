1cfe3e65aa0df685545c9f91063b5c82d9bbb95b
Eric Cormier <Eric@Pureconstructs.com>
2018-10-10 18:51:20 -0600

    feat(templates): completed template file structure
    - Added new file /generator/templates/module/Home.vue
    - Completed creation of main index.js routes file
    - Completed creation of module index.js routes file
    
    fixes(#2)

---
+++ generator/templates/module/index.js
@@ -0,0 +1,3 @@
+import moduleName from '@/views/moduleName'
+
+export default [{}, ]
---
+++ generator/templates/src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const baseRoutes = [
+
+]
+
+const routes = baseRoutes.concat(
+
+)
+
+export default new Router({
+  routes,
+})
